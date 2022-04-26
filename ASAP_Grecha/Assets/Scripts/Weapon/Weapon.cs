048bdc1747ad020d2e6b036353a487d99feab497
Brentimus <Gamepopins@gmail.com>
2022-04-26 18:29:12 +1000

    Cursor add

--- ASAP_Grecha/Assets/Scripts/Weapon/Weapon.cs
+++ ASAP_Grecha/Assets/Scripts/Weapon/Weapon.cs
@@ -6,6 +6,7 @@ public class Weapon : MonoBehaviour
 {
     public Transform _firePoint;
     public GameObject _bulletPrefab;
+    //public int timeDelayAttack = 2;
 
     private void Update()
     {
